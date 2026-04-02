658023e617be90a122fabc6537e6f01d112b7cb4
234912 <234912@KB37-118-C12>
2026-04-02 14:17:33 +0300

    Добавьте файлы проекта.

---
+++ KPr16/EcsLike.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace KPr16
+{
+    public class EntityNamed
+    {
+        public string name { get; set; }
+    }
+    public class EntityLiving: EntityNamed
+    {
+        public int hp { get; set; }
+    }
+    public class Event
+    {
+        public EntityNamed src { get; set; }
+        public EntityNamed dst { get; set; }
+    }
+    public interface Behaviour
+    {
+        void process(Event e);
+    }
+}
---
+++ KPr16/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using KPr16;
+
+namespace KPr16
+{
+    public class EventDamage: Event {}
+    public class Entity: EntityLiving
+    {
+        EntityNamed weapon;
+        EntityNamed armor;
+    }
+    
+    internal class Game
+    {
+        public static EntityLiving create_enemy(bool is_boss = false)
+        {
+            if (is_boss)
+                return new EntityLiving() { name = "The ___", hp = 69 };
+            else
+                return new EntityLiving() { name = "___", hp = 37 };
+        }
+    }
+}
